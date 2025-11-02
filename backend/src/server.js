615ec7fae8aadb9437e79faafa86806a32c634fa
hungtan1406 <hungtanth1406@gmail.com>
2025-11-03 00:24:27 +0700

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,19 +6,33 @@ import { connectDB } from './config/db.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { protectedRoute } from './middlewares/authMiddleware.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+}
+
 app.use(cookieParser());
 
 app.use('/api/auth', authRoute);
 app.use('/api/tasks', protectedRoute, tasksRoute);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
